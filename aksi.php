4cfcf2d34d3f010def6ec913ad2fd3fde62631b4
harissamiaji <55997486+harissamiaji@users.noreply.github.com>
2019-10-01 00:57:39 +0700

    Add files via upload

---
+++ aksi.php
@@ -0,0 +1,10 @@
+
+<?php
+$nama=$_GET["nama"]; 
+$kota=$_GET["kota"]; 
+echo "Selamat Datang : ".$nama;
+echo "<br>";
+echo "Asal Kota : ".$kota;
+echo "<br>";
+echo "Senang betemu dengan anda :)"
+?>
